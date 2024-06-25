37b733f48a4f8cc35b6f09f03098ec0e59fedde1
Jack Calcinius <163590820+travyyx@users.noreply.github.com>
2024-06-25 11:29:45 +0000

    "Update react-router-dom to 6.24.0, @remix-run/router to 1.17.0, and add router configuration in main.tsx with new MainPage component"

--- gouv-website-redesign/src/main.tsx
+++ gouv-website-redesign/src/main.tsx
@@ -2,10 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { MainPage } from './pages/main.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/home',
+    element: <MainPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
---
+++ gouv-website-redesign/src/pages/main.tsx
@@ -0,0 +1,7 @@
+export function MainPage() {
+    return (
+        <main className="bg-background">
+            <h1>Hello.</h1>
+        </main>
+    )
+}
