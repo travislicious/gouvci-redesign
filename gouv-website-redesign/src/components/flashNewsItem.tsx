ddff08c97de0bf3b3122035a5b4c311de901d5a3
Jack Calcinius <163590820+travyyx@users.noreply.github.com>
2024-06-27 09:10:04 +0000

    "Updated styles for FlashNewsItem component: changed background colors, added hover effects, and modified text colors."

--- gouv-website-redesign/src/components/flashNewsItem.tsx
+++ gouv-website-redesign/src/components/flashNewsItem.tsx
@@ -1,9 +1,9 @@
 export function FlashNewsItem() {
     return (
         <div className="border-[#5ead3d] w-full md:w-[30rem] bg-transparent xl:h-96 border xl:w-full md:h-64">
-            <div className="w-full h-full bg-[#76d84c] p-2 hover:bg-[#4c8d31] transition-colors duration-200 cursor-pointer">
-            <h1 className="text-2xl text-white mb-1">News Title</h1>
-            <h1 className="text-lg text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore enim placeat, ullam ipsum numquam dolore asperiores accusantium? Ab doloremque laborum laudantium quasi, quidem ut odio voluptatem esse, suscipit ipsa totam.</h1>    
+            <div className="w-full h-full bg-[#6eca47] p-2 hover:bg-[#549e34] transition-colors duration-200 cursor-pointer group">
+            <h1 className="text-2xl text-white mb-1 group-hover:text-white/80 transition-colors duration-200 ">News Title</h1>
+            <h1 className="text-lg text-white group-hover:text-white/80 transition-colors duration-200 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore enim placeat, ullam ipsum numquam dolore asperiores accusantium? Ab doloremque laborum laudantium quasi, quidem ut odio voluptatem esse, suscipit ipsa totam.</h1>    
             </div>
         </div>
     )
