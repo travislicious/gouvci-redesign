bd294e1e8a040c80e16f374c4d45e551ae4fd73a
Jack Calcinius <163590820+travyyx@users.noreply.github.com>
2024-07-10 17:19:55 +0000

    "Updated CSS classes in AtOneItem, FeatureItem, and NewsItem components"

--- gouv-website-redesign/src/components/FeatureItem.tsx
+++ gouv-website-redesign/src/components/FeatureItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export const FeatureItem: React.FC<Props> = ({name, icon, description}) => {
     return (
-        <div className="w-[27rem] bg-[#FFB534] h-72 md:w-full p-3 flex flex-col items-center text-neutral-100">
+        <div className="w-[27rem] bg-[#FFB534] h-72 md:w-full p-3 flex flex-col items-center text-neutral-100 hover:scale-90 transition-all duration-200">
             { icon }
             <a className="w-full text-center text-2xl mt-2" href='#'>{name}</a>
             <h1 className='text-lg'>{description}</h1>
