96dd7a3b862b65797a482b68dcc2309bd208f83c
Jack Calcinius <163590820+travyyx@users.noreply.github.com>
2024-07-10 17:16:29 +0000

    "Updated styles for DocItem and DocsList components"

--- gouv-website-redesign/src/components/DocItem.tsx
+++ gouv-website-redesign/src/components/DocItem.tsx
@@ -1,6 +1,6 @@
 export const DocItem = () => {
     return (
-        <div className="bg-blue-500 w-full p-2 text-white flex gap-2 justify-between">
+        <div className="border border-[#6fc949] w-full p-2 text-white flex gap-2 justify-between hover:bg-[#6fc949] hover:scale-105 transition-all duration-200">
             <h1>Nom Du Document.</h1>
             <div>000</div>
         </div>
