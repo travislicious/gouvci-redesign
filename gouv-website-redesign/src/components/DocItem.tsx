3c3c1d9b3dcbc1009378b290e1b837f12f6c06bc
Jack Calcinius <163590820+travyyx@users.noreply.github.com>
2024-07-12 13:56:21 +0000

    "Updated DocItem and FocusItem components: changed text content, added link and cursor pointer styles."

--- gouv-website-redesign/src/components/DocItem.tsx
+++ gouv-website-redesign/src/components/DocItem.tsx
@@ -1,8 +1,8 @@
 export const DocItem = () => {
     return (
         <div className="border border-[#1ebe81] w-full p-2 text-white flex gap-2 justify-between hover:bg-[#1ebe81] hover:scale-105 transition-all duration-200">
-            <h1>Nom Du Document.</h1>
-            <div>000</div>
+            <a className="w-full truncate" href="#">Nom Du Document.</a>
+            <div>01/01/70</div>
         </div>
     )
 }
--- gouv-website-redesign/src/components/FocusItem.tsx
+++ gouv-website-redesign/src/components/FocusItem.tsx
@@ -3,7 +3,7 @@ export function FocusItem() {
         <div className="w-full bg-[#F57F01] p-4 text-background">
             <div className="w-full flex items-center justify-between">
                 <h1 className="text-3xl font-semibold border-l-4 pl-2 border-background border-[#fab163]">Focus.</h1>
-                <h1 className="text-xl">1/4</h1>
+                <h1 className="text-xl cursor-pointer">1/4</h1>
             </div>
             <div>
                 <h1 className="my-4 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum odit saepe optio. Totam, quam dolore facere ducimus, accusantium repellat earum quod impedit, blanditiis quos velit illo corrupti quidem necessitatibus similique.</h1>
